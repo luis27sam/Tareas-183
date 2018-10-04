0601558f165d34bdea75729ddfe36413d0f769aa
Arnold <angv@hotmail.es>
2018-10-04 16:33:44 -0500

    Barra de Navegacion, Metodos de Eliminar y Editar

--- app/controllers/tareas_controller.rb
+++ app/controllers/tareas_controller.rb
@@ -10,7 +10,8 @@ class TareasController < ApplicationController
   def create
   	@tarea = Tarea.new(titulo: params[:tarea][:titulo], descripcion: params[:tarea][:descripcion])
   	if @tarea.save
-  	else render :new
+  	redirect_to controller: 'tareas', action: 'show', id: @tarea.id 
+    else render :new
   	end
   end
 
@@ -18,4 +19,16 @@ class TareasController < ApplicationController
   	@tarea = Tarea.find(params[:id])
   	#insert into tareas (titulo,descripcion) VALUES (FORMULARIO)
   end
+
+  def destroy
+    @tarea = Tarea.find(params[:id])
+    @tarea.destroy
+    redirect_to controllers: "tareas",action: "index"
+  end
+
+  def edit
+    @tarea = Tarea.find(params[:id])
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  get 'tareas', to: 'tareas#index'
-  get 'tareas/new', to: 'tareas#new'
-  post 'tareas', to: 'tareas#create'
+  get 'tareas',to: 'tareas#index'
+  get 'tareas/new',to: 'tareas#new'
+  post 'tareas',to: 'tareas#create'
   get 'bienvenido/index'
-  get 'tareas/:id', to: 'tareas#show'
+  get 'tareas/:id',to: 'tareas#show'
+  delete 'tareas/:id',to: 'tareas#destroy'
+  get 'tareas/:id/edit', to: 'tareas/edit'
+  put 'tareas/:id',to: 'tareas#update'
 
   root 'bienvenido#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
